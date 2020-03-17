46b851da950b3351d34019515ab8c55e95467a76
jodch <josedavidchiquillo@gmail.com>
2020-03-17 16:23:06 -0300

    frontinicial

---
+++ front/components/Main.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { Route, Redirect, Switch } from "react-router-dom";
+
+import PrincipalContainer from "../containers/PrincipalContainer";
+
+import { connect } from "react-redux"
+
+
+const mapStateToProps = (state) => {
+  return {
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+   
+  }
+}
+
+class Main extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+
+  render() {
+    return (
+      <div id="main">
+        <Switch>
+          <Route path="/home" exact component={PrincipalContainer} />
+          <Redirect from="/" to="/home" />
+        </Switch>
+      </div>
+    );
+  }
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Main)
