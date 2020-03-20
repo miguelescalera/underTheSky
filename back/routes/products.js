fa9d5dfc056d78cd18fce4c1978f6386b2be7e03
fernandezAlan <alan.fer09@gmail.com>
2020-03-20 10:54:25 -0700

    rutas

--- back/routes/products.js
+++ back/routes/products.js
@@ -6,44 +6,74 @@ const Order = require ("../models/order")
 const Frame = require("../models/frame")
 const Size = require("../models/size")
 const Style = require("../models/style")
+const UserData = require("../models/userData")
 
 
 router.post("/newProduct",function(req,res){
-   
-    User.findByPk(req.user.id)
-    .then((user)=>{
-        
-        Product.create(req.body)
-        .then((product)=>{
-                product.setUser(user)
-        })
-        .then(()=>{
-            Order.create(req.body)
-        })
-        .then((order)=>{
-            Product.setOrder(order)
-        }).then(()=>{
-            Frame.findByPk(req.body.frameId)
-            .then((frame)=>{
-                Product.setFrame(frame)
+    Product.findOrCreate({where:{
+        frameId: req.body.frameId,
+        sizeId: req.body.sizeId,
+        styleId: req.body.sizeId
+    }
+}).then((product)=>{
+        User.findByPk(req.user.id)
+        .then((user)=>{
+            UserData.create()
+            .then((userData)=>{
+                Order.findOrCreate({
+                    where:{
+                        userId: req.user.id,
+                        status:req.body.status,
+                        address: req.body.address
+                    }
+                }).then((order)=>{
+                    userData.setOrder(order)
+                    userData.setUser(user)
+                    userData.setProduct(product)
+                })
+              })
             })
         })
-        .then(()=>{
-            Size.findByPk(req.body.sizeId)
-            .then((size)=>{
-                Product.setFrame(size)
-            })
-        })
-        .then(()=>{
-            Style.findByPk(req.body.styleId)
-            .then((style)=>{
-                Product.setFrame(style)
-            })
-        })
-    })
-    .then(()=>{
+    }).then(()=>{
         res.sendStatus(200)
-    })
+    }).catch(error=>console.error(error))
+    
+   /*
+   User.findByPk(req.user.id)
+   .then((user)=>{
+       
+       Product.create(req.body)
+       .then((product)=>{
+               product.setUser(user)
+       })
+       .then(()=>{
+           Order.create(req.body)
+       })
+       .then((order)=>{
+           Product.setOrder(order)
+       }).then(()=>{
+           Frame.findByPk(req.body.frameId)
+           .then((frame)=>{
+               Product.setFrame(frame)
+           })
+       })
+       .then(()=>{
+           Size.findByPk(req.body.sizeId)
+           .then((size)=>{
+               Product.setFrame(size)
+           })
+       })
+       .then(()=>{
+           Style.findByPk(req.body.styleId)
+           .then((style)=>{
+               Product.setFrame(style)
+           })
+       })
+   })
+   .then(()=>{
+       res.sendStatus(200)
+   })
+   */
 })
 
 
