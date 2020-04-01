7848745ffe237a8d8db3f804372a032a1c79bd48
fernandezAlan <alan.fer09@gmail.com>
2020-04-01 14:40:04 -0700

    modificando el modelo de orders

--- back/models/order.js
+++ back/models/order.js
@@ -9,25 +9,29 @@ Order.init(
             type: Sequelize.ENUM('cart', 'delivered', 'inprocess', 'printing'),
             defaultValue: 'inprocess'
         },
+        deliveryPoint:{
+            type:Sequelize.BOOLEAN,
+            defaultValue:true
+        },
         address: {
             type: Sequelize.STRING,
-            allowNull: false
+          
         },
         city:{
             type:Sequelize.STRING,
-            allowNull: false
+         
         },
         country:{
             type:Sequelize.STRING,
-            allowNull: false
+            
         },
         state:{
             type:Sequelize.STRING,
-            allowNull: false
+           
         },
         postCode:{
             type:Sequelize.STRING,
-            allowNull: false
+           
         },
         transactionNumber:{
             type:Sequelize.INTEGER,
